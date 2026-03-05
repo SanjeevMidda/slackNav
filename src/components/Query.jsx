a854501e5c2d445b711d5b9a15fd7fb26fd69dc9
Sanjeev Midda <s.midda1@gmail.com>
2026-03-05 16:56:40 +0000

    refactored UI for Query component for better readability

--- src/components/Query.jsx
+++ src/components/Query.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Query = () => {
-  return <div>Query</div>;
+  return (
+    <>
+      <div className="queryContainer">
+        <div className="queryStatus"></div>
+        <input type="text" />
+        <div className="result"></div>
+      </div>
+      <p>Raise on Card-Ops channel</p>
+    </>
+  );
 };
 
 export default Query;
